5287e9901098cacbdfa768872af70f2ca8236f02
Žan Tramte <zan.tramte@gmail.com>
2021-02-25 17:09:44 +0100

    Adding news to my website!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_162423) do
+ActiveRecord::Schema.define(version: 2021_02_25_155419) do
+
+  create_table "news", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 # Could not dump table "posts" because of following StandardError
 #   Unknown type 'reference' for column 'user_id'
