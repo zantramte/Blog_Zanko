8a6f7b37c9750daf5bf3331ce4cfbb81aa4904be
Žan Tramte <zan.tramte@gmail.com>
2021-02-25 17:20:43 +0100

    Adding some css to my website - background linear gradient color.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -25,7 +25,7 @@ class NewsController < ApplicationController
 
     respond_to do |format|
       if @news.save
-        format.html { redirect_to @news, alert: "News was successfully created." }
+        format.html { redirect_to @news, notice: "News was successfully created." }
         format.json { render :show, status: :created, location: @news }
       else
         format.html { render :new, status: :unprocessable_entity }
