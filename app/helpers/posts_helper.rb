3793c0048875610ac95ebf54ab060c7c7057c9b9
Žan Tramte <zan.tramte@gmail.com>
2021-02-27 17:16:09 +0100

    Adding html safe function to function in post helper ruby file (.rb).

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,5 +3,6 @@ module PostsHelper
     html = "<td>#{link_to 'Show', post}</td>"
     html += "<td>#{link_to 'Edit', edit_post_path(post) if current_user == post.user}</td>"
     html += "<td>#{link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' } if current_user == post.user}</td>"
+    html.html_safe
   end
 end
