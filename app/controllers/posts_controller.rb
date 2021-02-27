ff6cd9b27e2b1525c48ccc96dd6d3ed6af98cb18
Žan Tramte <zan.tramte@gmail.com>
2021-02-27 16:35:48 +0100

    Authenticate user and adding simple navbar!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
-  before_action :authenticat_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /posts or /posts.json
   def index
