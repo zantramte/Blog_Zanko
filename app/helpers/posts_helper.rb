7245a16452c2921aa1e596bb8aa6a279bf89a82a
Žan Tramte <zan.tramte@gmail.com>
2021-02-27 17:14:35 +0100

    Adding function to display link on post website with Post helper.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,7 @@
 module PostsHelper
+  def display_post_links(post)
+    html = "<td>#{link_to 'Show', post}</td>"
+    html += "<td>#{link_to 'Edit', edit_post_path(post) if current_user == post.user}</td>"
+    html += "<td>#{link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' } if current_user == post.user}</td>"
+  end
 end
