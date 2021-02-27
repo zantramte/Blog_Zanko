2e93f18fa25f0235b8431729a0276e3f7d8fce80
Žan Tramte <zan.tramte@gmail.com>
2021-02-27 17:25:21 +0100

    Adding another function to post website for better user experience!

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,4 +5,11 @@ module PostsHelper
     html += "<td>#{link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' } if current_user == post.user}</td>"
     html.html_safe
   end
-end
+
+  def display_post_show_links
+      html = link_to 'Edit', edit_post_path(post) if current_user == post.user}
+      html += ' | '
+      html += link_to 'Back', posts_path
+      html.html_safe
+    end
+  end
