4188c4ebc916e4e37e2d6e313479be2b5c70a53e
Žan Tramte <zan.tramte@gmail.com>
2021-02-27 16:26:32 +0100

    Adding navbar menu to my Ruby website!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticat_user!
 
   # GET /posts or /posts.json
   def index
