5446d4509899e1eabae89ec658845cb8d177ddbb
Žan Tramte <zan.tramte@gmail.com>
2021-02-28 10:46:42 +0100

    Displaying author name on posts, and also when the user create post!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,16 @@
 module ApplicationHelper
-  def display_navbar_options
+  def display_session_links
     capture do
       if user_signed_in?
         concat link_to "Uredi profil", edit_user_registration_path
+        concat " "
         concat link_to "Odjava", destroy_user_session_path, method: :delete
       else
         concat link_to "Prijava", new_user_session_path
         concat " "
         concat link_to "Registracija", new_user_registration_path
         concat " "
+      end
     end
   end
 end
