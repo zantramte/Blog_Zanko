dd8b3f6bb86bde6654974812790fe555311448a3
Žan Tramte <zan.tramte@gmail.com>
2021-02-28 10:28:33 +0100

    Designing navbar menu on my website!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 module ApplicationHelper
   def display_navbar_options
-    html = link_to "Domov", root_path
-    html += link_to "Prijava", new_user_session_path unless user_signed_in?
-    html += link_to "Registracija", new_user_registration_path unless user_signed_in?
-    html += link_to "Odjava", destroy_user_session_path, method: :delete if user_signed_in?
-    html.html_safe
+    capture do
+      if user_signed_in?
+        concat link_to "Uredi profil", edit_user_registration_path
+        concat link_to "Odjava", destroy_user_session_path, method: :delete
+      else
+        concat link_to "Prijava", new_user_session_path
+        concat " "
+        concat link_to "Registracija", new_user_registration_path
+        concat " "
+    end
   end
 end
