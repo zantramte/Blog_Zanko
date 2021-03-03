179383529d0c37b2154182ab80e3a190aa15b996
Žan Tramte <zan.tramte@gmail.com>
2021-03-03 17:37:23 +0100

    Changing settings in code in environment.js file.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.append(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
+environment.plugins.append(‘Provide’, new webpack.ProvidePlugin({
+ $: ‘jquery’,
+ jQuery: ‘jquery’,
+ Popper: [‘popper.js’, ‘default’]
+}));
+const config = environment.toWebpackConfig();
+config.resolve.alias = {
+ jquery: ‘jquery/src/jquery’
+};
 
 module.exports = environment
