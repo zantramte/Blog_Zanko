d12097057133a413556a85c27ea79cd95ce80831
Žan Tramte <zan.tramte@gmail.com>
2021-03-06 20:52:32 +0100

    Removing last two links, because it is not necessary that they are displayed on posts index website.

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,2 +1,18 @@
 module NewsHelper
+  def display_index_post_links(post)
+    capture do
+      concat link_to('Preberi več', post, class: 'streched-link')
+      end
+    end
+  end
+
+  def display_show_post_links(post)
+    capture do
+      if current_user == post.user
+        concat link_to 'Edit', edit_post_path(post)
+        concat ' | '
+      end
+      concat link_to 'Back', posts_path
+    end
+  end
 end
