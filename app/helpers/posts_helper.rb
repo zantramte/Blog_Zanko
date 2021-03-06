a5bcca7985c84ae97013ef3b43184c7aefc3bb10
Žan Tramte <zan.tramte@gmail.com>
2021-03-06 20:15:33 +0100

    Adding new title for users in Slovenian language.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   def display_index_post_links(post)
     capture do
-      concat link_to('Show', post, class: 'streched-link')
+      concat link_to('Preberi več', post, class: 'streched-link')
       if current_user == post.user
         concat link_to('Edit', edit_post_path(post))
         concat link_to('Destroy', post, method: :delete, data: { confirm: 'Are you sure?' })
