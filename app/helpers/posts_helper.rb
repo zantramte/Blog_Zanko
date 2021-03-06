4b16902d431594c43bb09e6beea932aa8cbc8c7a
Žan Tramte <zan.tramte@gmail.com>
2021-03-06 20:12:37 +0100

    Adding new desing and functionality to my website in posts_helper.rb.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,20 +1,21 @@
 module PostsHelper
   def display_index_post_links(post)
-    html = "<td>#{link_to 'Show', post}</td>"
-     if current_user == post.user
-       html += "<td>#{link_to 'Edit', edit_post_path(post)}</td>"
-       html += "<td>#{link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }}</td>"
+    capture do
+      concat link_to('Show', post, class: 'streched-link')
+      if current_user == post.user
+        concat content_tag(:td, link_to('Edit', edit_post_path(post)))
+        concat content_tag(:td, link_to('Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }))
+      end
     end
-    html.html_safe
   end
 
   def display_show_post_links(post)
-    html = ""
-    if current_user == post.user
-      html = link_to 'Edit', edit_post_path(post)
-      html += ' | '
-    end
-      html += link_to 'Back', posts_path
-      html.html_safe
+    capture do
+      if current_user == post.user
+        concat link_to 'Edit', edit_post_path(post)
+        concat ' | '
+      end
+      concat link_to 'Back', posts_path
     end
   end
+end
